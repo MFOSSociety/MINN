44d5f22c40ab8a8c6b973626bc04242a5c2e7537
Piyush Raj <piyush.189303055@muj.manipal.edu>
2019-12-27 23:38:45 +0530

    init: project website

---
+++ docs/script.js
@@ -0,0 +1,9 @@
+// Notes:
+
+// Use a combination of perspective size and transform rotateX to set street angle (affects whole scene)
+
+// Height of gps-bg__bg should be 2x the absolute value of the translateY distance
+
+// Top of gps-bg__bg should be 25% the absolute value of the translateY distance
+
+// Top and bottom of gps-bg__route should be the same as top of gps-bg__bg
